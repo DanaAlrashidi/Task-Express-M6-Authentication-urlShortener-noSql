8647174e23e79a087311a78178730e902e9e8894
loginn <loginn>
2024-01-24 20:23:05 +0300

    done

---
+++ api/auth/controllers.js
@@ -0,0 +1,45 @@
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const hashPass = async (password) => {
+  const hashPass = await bcrypt.hash(password, 10);
+  return hashPass;
+};
+
+const getAllUsers = async (req, res, next) => {
+  try {
+    const user = await User.find();
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+const register = async (req, res, next) => {
+  try {
+    const password = req.body.password;
+    const hashPassword = await hashPass(password);
+    req.body.password = hashPassword;
+    const user = await User.create(req.body);
+
+    const payload = {
+      _id: user._id,
+      username: user.username,
+    };
+    const token = jwt.sign(payload, proces.env.SECRET_PASS, {
+      expiresIn: "4h",
+    });
+    return res.status(201).json({ token: token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    return res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, register, login };
--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+const PORT = 8002;
+
 const express = require("express");
 const connectDb = require("./database");
 const urlRoutes = require("./api/urls/urls.routes");
 const userRoutes = require("./api/users/users.routes");
 const notFoundHandler = require("./middlewares/notFoundHandler");
 const errorHandler = require("./middlewares/errorHandler");
+const authRouter = require("./api/auth/routes");
+const morgan = require("morgan");
+const passport = require("passport");
+const localStrategy = require("./middlewares/passport");
 
 const app = express();
 connectDb();
 
 app.use(express.json());
-
+app.use(morgan("dev"));
+app.use(passport.initialize());
+passport.use("local", localStrategy);
 app.use("/urls", urlRoutes);
 app.use(userRoutes);
+
+console.log(__dirname);
+app.use("/api/auth", authRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
+app.listen(8002, () => {
+  console.log(`The application is running on localhost${PORT}`);
 });
